73a1c715714c3d44e39fa19323407a31884a6203
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-09-16 18:42:54 +0530

    socket actions for user (join and disconnect)

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,6 +28,60 @@ const io = new Server(server, {
   pingTimeout: 60000,
 });
 
+let userSocketMap: User[] = [];
+
+const getUsersInRoom = (roomId: string): User[] => {
+  return userSocketMap.filter((user) => user.roomId === roomId);
+};
+
+const getUserBySocketId = (socketId: SocketId): User | null => {
+  const user = userSocketMap.find((user) => user.socketId === socketId);
+  if (!user) {
+    console.error("User not found for socket ID:", socketId);
+    return null;
+  }
+  return user;
+};
+
+io.on("connection", (socket) => {
+  // User Actions
+  socket.on(SocketEvent.JOIN_REQUEST, ({ roomId, username }) => {
+    const isUsernameExist = getUsersInRoom(roomId).filter(
+      (u) => u.username === username
+    );
+
+    if (isUsernameExist.length > 0) {
+      io.to(socket.id).emit(SocketEvent.USERNAME_EXISTS);
+      return;
+    }
+
+    const user = {
+      username,
+      roomId,
+      status: USER_CONNECTION_STATUS.ONLINE,
+      cursorPosition: 0,
+      typing: false,
+      socketId: socket.id,
+      currentFile: null,
+    };
+
+    userSocketMap.push(user);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit(SocketEvent.USER_JOINED, { user });
+    const users = getUsersInRoom(roomId);
+    io.to(socket.id).emit(SocketEvent.JOIN_ACCEPTED, { user, users });
+  });
+
+  socket.on("disconnectiong", () => {
+    const user = getUserBySocketId(socket.id);
+    if (!user) return;
+    const roomId = user.roomId;
+    socket.broadcast.to(roomId).emit(SocketEvent.USER_DISCONNECTED, { user });
+    userSocketMap = userSocketMap.filter((u) => u.socketId !== socket.id);
+    socket.leave(roomId);
+  });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
