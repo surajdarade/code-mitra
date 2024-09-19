364d02fd1ae1c9f6a9b4cd7730923a5496b03782
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-09-19 15:57:10 +0530

    tested client done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import GitHubCorner from "./components/GitHubCorner"
+import Toast from "./components/toast/Toast"
+import EditorPage from "./pages/EditorPage"
+import HomePage from "./pages/HomePage"
+
+const App = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/editor/:roomId" element={<EditorPage />} />
+                </Routes>
+            </Router>
+            <Toast /> {/* Toast component from react-hot-toast */}
+            <GitHubCorner />
+        </>
+    )
 }
+
+export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import AppProvider from "./context/AppProvider.tsx"
+import "@/styles/global.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    // <React.StrictMode>
+    <AppProvider>
+        <App />
+    </AppProvider>,
+    // </React.StrictMode>
 )
